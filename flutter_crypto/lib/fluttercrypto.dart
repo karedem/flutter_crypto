dfc331b19963b867251472970060632e00ee8950
wangxin <wx@qtwo.fun>
2020-05-15 14:12:41 +0800

    初始化

---
+++ flutter_crypto/lib/fluttercrypto.dart
@@ -0,0 +1,19 @@
+library fluttercrypto;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
+
+class AES {}
+
+class DES {}
+
+class SM4 {}
+
+class RSA {}
+
+class SM2 {}
+
+class SHA1 {}
